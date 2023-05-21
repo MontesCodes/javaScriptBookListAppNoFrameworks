3c2073de6ca30a9d2c3a0e96cffdefd595b2de01
MontesCodes <MontiesWebDev@GMail.com>
2023-05-20 23:34:30 -0400

    Time -> 18:33, engineeringMeDesktop, 05-20-2023

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,53 @@
 'use strict';
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+      {
+        title: 'Book One',
+        author: 'John Doe',
+        isbn: '3434434',
+      },
+      {
+        title: 'Book Two',
+        author: 'John Doe',
+        isbn: '5434435',
+      },
+    ];
+
+    const books = StoredBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
+
+// Store Class: Handles Storage
+
+// Even: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Even: Add a Book
+
+// Even: Remove a Book
