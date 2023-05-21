0e2026de9707075928477d6d009fd0d45a77e0df
MontiesCodingFrontEndDev <MontiesWebDev@Gmail.com>
2023-05-21 00:12:24 -0400

    Time Stamp 25:46, MacBookProLaptop 5/21/2023

--- JS/app.js
+++ JS/app.js
@@ -41,6 +41,12 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+  }
 }
 
 // Store Class: Handles Storage
@@ -50,8 +56,23 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Even: Add a Book
 document.querySelector('#book-form').addEventListener('submit', e => {
+  // Prevent actual submit
+  e.preventDefault();
+
   // Get form values
-  const title = document.querySelector();
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+
+  // Instatiate book
+  const book = new Book(title, author, isbn);
+
+  // console.log(book);
+  // Add book to UI
+  UI.addBookToList(book);
+
+  // Clear Fields
+  UI.clearFields();
 });
 
 // Even: Remove a Book
